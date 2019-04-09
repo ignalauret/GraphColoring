04ef1ef3a6814c96e6b4ea3d6e4b1cbf930c7651
ignalauret <ignalau@gmail.com>
2019-04-09 08:56:48 -0300

    Greedy Arreglado

--- Source Code/Greedy.c
+++ Source Code/Greedy.c
@@ -1,28 +1,26 @@
 #include "Rii.h"
 
 u32 Greedy(Grafo G){
+  ResetColores(G);
   u32 nVertices = G->nVertices;
-  Vertice verticesUsados[nVertices];
   u32 nColores = 0;
+
   /* Main Loop, una vez por cada vertice. */
   for(u32 i = 0; i<nVertices; i++){
     Vertice vertice = G->vertices[i];
     bool coloresUsados[nColores+1];
     /* Inicializo arreglo de bools de colores usados en false. */
-    for(u32 j = 0; j<nColores+1; j++){
-      coloresUsados[j] = false;
-    }
+    for(u32 j = 0; j<nColores+1; j++) coloresUsados[j] = false;
     /* Seteo los colores usados por los vecinos a True y el resto queda en False. */
-    for(u32 h = 0; h<i; h++){
-      Vertice verticeObservado = verticesUsados[h];
-      coloresUsados[verticeObservado->color] |= EsVecino(vertice,verticeObservado);
+    for(u32 g = 0; g < vertice->grado; g++){
+      u32 colorObs = vertice->vecinos[g]->color;
+      if(colorObs != (u32)-1) coloresUsados[colorObs] = true;
     }
-    u32 color = 0;
     /* Busco el primer color que este en false o hasta llegar al ultimo color. */
+    u32 color = 0;
     while(coloresUsados[color] && color<nColores) color++;
     /* Pinto el vertice y lo agrego en los vertices usados. */
     vertice->color = color;
-    verticesUsados[i] = vertice;
     /* Si lo pinte del ultimo color, agregue un nuevo color por lo tanto incremento el contador. */
     if(color == nColores) nColores++;
   }
--- Source Code/main.c
+++ Source Code/main.c
@@ -16,6 +16,7 @@ int main(void){
   printf("Grafo Creado en %f segundos\n", segundos );
 
   /********************** Analisis del grafo **********************/
+  if(Bipartito(grafo)) printf("Es Bipartito\n");
   OrdenNatural(grafo);
   u32 colores = Greedy(grafo);
   printf("Greedy con Orden Natural pinto con %u colores\n", colores);
@@ -24,13 +25,13 @@ int main(void){
   colores = Greedy(grafo);
   printf("Greedy con Orden WelshPowell pinto con %u colores\n", colores);
 
-  tiempoInicio = clock();
-  for(uint i = 0; i < 300; i++){
-    Greedy(grafo);
-  }
-  tiempoFinal = clock();
-  segundos = (double)(tiempoFinal - tiempoInicio) / CLOCKS_PER_SEC;
-  printf("300 Greedy en %f segundos\n", segundos );
+  // tiempoInicio = clock();
+  // for(uint i = 0; i < 3000; i++){
+  //   Greedy(grafo);
+  // }
+  // tiempoFinal = clock();
+  // segundos = (double)(tiempoFinal - tiempoInicio) / CLOCKS_PER_SEC;
+  // printf("3000 Greedy en %f segundos\n", segundos );
 
   DestruirGrafo(grafo);
   return 0;
