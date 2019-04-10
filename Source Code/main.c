d9ddf127f8d126336409ac9a69d0c48eca940a4a
ignalauret <ignalau@gmail.com>
2019-04-09 23:15:12 -0300

    Testeo

--- Source Code/main.c
+++ Source Code/main.c
@@ -21,24 +21,22 @@ int main(void){
   u32 colores = Greedy(grafo);
   printf("Greedy con Orden Natural pinto con %u colores\n", colores);
 
-  OrdenWelshPowell(grafo);
+  // OrdenWelshPowell(grafo);
+  // colores = Greedy(grafo);
+  // printf("Greedy con Orden WelshPowell pinto con %u colores\n", colores);
+
+  tiempoInicio = clock();
+  for(u32 i = 0; i < 1000; i++){
+    OrdenRMBCnormal(grafo);
+    Greedy(grafo);
+  }
+  tiempoFinal = clock();
+  segundos = (double)(tiempoFinal - tiempoInicio) / CLOCKS_PER_SEC;
   colores = Greedy(grafo);
-  printf("Greedy con Orden WelshPowell pinto con %u colores\n", colores);
-
-  // tiempoInicio = clock();
-  // for(u32 i = 0; i < 3000; i++){
-  //   OrdenRMBCnormal(grafo);
-  //   Greedy(grafo);
-  // }
-
-  OrdenRMBCnormal(grafo);
-  u32 color = Greedy(grafo);
-  printf("3102 Greedy en %f segundos con %u colores\n", segundos, color );
+  printf("1000 Greedy en %f segundos con %u colores\n", segundos, colores );
 
 
 
-  // tiempoFinal = clock();
-  // segundos = (double)(tiempoFinal - tiempoInicio) / CLOCKS_PER_SEC;
 
   DestruirGrafo(grafo);
   return 0;
