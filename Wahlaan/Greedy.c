be6f957c0a6de8236751ef1d0bee518dec72c90e
ignalauret <ignalau@gmail.com>
2019-04-22 22:44:46 -0300

    Greedy intento arreglo

--- Wahlaan/Greedy.c
+++ Wahlaan/Greedy.c
@@ -4,20 +4,22 @@ u32 Greedy(Grafo G){
   ResetColores(G);
   u32 nVertices = G->nVertices;
   u32 nColores = 0;
+  bool coloresUsados[nVertices];
+  u32 colorObs;
+  u32 color;
 
   /* Main Loop, una vez por cada vertice. */
   for(u32 i = 0; i<nVertices; i++){
     Vertice vertice = G->vertices[i];
-    bool coloresUsados[nColores+1];
     /* Inicializo arreglo de bools de colores usados en false. */
     for(u32 j = 0; j<nColores+1; j++) coloresUsados[j] = false;
     /* Seteo los colores usados por los vecinos a True y el resto queda en False. */
     for(u32 g = 0; g < vertice->grado; g++){
-      u32 colorObs = vertice->vecinos[g]->color;
+      colorObs = vertice->vecinos[g]->color;
       if(colorObs != (u32)-1) coloresUsados[colorObs] = true;
     }
     /* Busco el primer color que este en false o hasta llegar al ultimo color. */
-    u32 color = 0;
+    color = 0;
     while(coloresUsados[color] && color<nColores) color++;
     /* Pinto el vertice y lo agrego en los vertices usados. */
     vertice->color = color;
